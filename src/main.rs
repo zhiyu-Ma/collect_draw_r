d9febbf0408d24542ec6a50f299a4b806d1f17b6
yangrudan <1549029765@qq.com>
2025-06-04 10:50:02 +0800

    use java palette

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
 use std::fs::File;
-use std::io::{BufRead, BufReader};
-use inferno::flamegraph::{self, Options};
+use std::io::BufReader;
+use inferno::flamegraph::{self, Options, Palette};
 
 fn main() {
-    // 替换为你的txt文件路径
     let file_path = "stacks.txt";
     let file = File::open(file_path).expect("Failed to open file");
     let reader = BufReader::new(file);
 
     let mut options = Options::default();
-    // 可以根据需要设置更多选项
+    options.colors = Palette::Multi(flamegraph::color::MultiPalette::Java);
     // options.colors = inferno::flamegraph::color::Palette::from_str("hot").unwrap();
 
     let mut output_file = File::create("flamegraph.svg").expect("Failed to create SVG file");
